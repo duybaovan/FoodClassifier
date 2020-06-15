a96d4c068e707f64dd5d9468dbb26ace9381b6eb
Bao Van <duybaovan@gmail.com>
2020-06-16 01:03:20 +0700

    loaded an image

---
+++ MemClassifier/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainViewController.swift
+//  MemClassifier
+//
+//  Created by Bao Van on 6/16/20.
+//  Copyright © 2020 Bao Van. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func chooseImagesButtonClicked(_ sender: Any) {
+       imagePicker.allowsEditing = false
+       imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+     
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
