12ed3f76c6641aa2c227cdc95826b5f6d39b5341
Bao Van <duybaovan@gmail.com>
2020-06-16 20:22:10 +0700

    basic single image classification working

--- MemClassifier/MainViewController.swift
+++ MemClassifier/MainViewController.swift
@@ -7,16 +7,100 @@
 //
 
 import UIKit
+import Vision
+import CoreML
+import ImageIO
+
 
 class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
+    let model = Resnet50FP16()
+    @IBOutlet weak var predictionLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            /*
+             Use the Swift class `MobileNet` Core ML generates from the model.
+             To use a different Core ML classifier model, add it to the project
+             and replace `MobileNet` with that model's generated Swift class.
+             */
+            let model = try VNCoreMLModel(for: Resnet50FP16().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        predictionLabel.text = "Classifying..."
+        let orientationNum: UInt32 = UInt32(image.imageOrientation.rawValue)
+        let orientation = CGImagePropertyOrientation(rawValue: orientationNum)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation!)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.predictionLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+        
+            if classifications.isEmpty {
+                self.predictionLabel.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(2)
+                let descriptions = topClassifications.map { classification in
+                    
+                    // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+                   return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                self.predictionLabel.text = "Classification:" + descriptions.joined(separator: "\n")
+            }
+        }
+    }
     
     @IBAction func chooseImagesButtonClicked(_ sender: Any) {
        imagePicker.allowsEditing = false
        imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func sceneLabel (forImage image: UIImage) -> String? {
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!){
+            guard let scene = try? model.prediction(image: pixelBuffer) else {
+                fatalError("unexpected")
+            }
+            return scene.classLabel
+        }
+        return nil
     }
     
     override func viewDidLoad() {
@@ -31,7 +115,7 @@ class MainViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             imageView.contentMode = .scaleAspectFit
             imageView.image = pickedImage
         }
-     
+        updateClassifications(for: imageView.image!)
         dismiss(animated: true, completion: nil)
     }
     
